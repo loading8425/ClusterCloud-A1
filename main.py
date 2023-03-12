e91c2210cb6c36dff71a89d8b92f794e4d056a5d
Mingyang LIU <szej18@gmail.com>
2023-03-12 17:02:18 +1100

    Update json

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
-from mpi4py import MPI
+# from mpi4py import MPI
+import json
+from utils import split_json_file
 
-comm = MPI.COMM_WORLD
-rank = comm.Get_rank()
+# comm = MPI.COMM_WORLD
+# rank = comm.Get_rank()
 
-if rank == 0:
-    data = {'a': 7, 'b': 3.14}
-    comm.send(data, dest=1, tag=11)
-elif rank == 1:
-    data = comm.recv(source=0, tag=11)
+#split_json_file('twitter-data-small.json','small',1000)
+
+with open('twitter-data-small.json','r', encoding="utf-8") as f:
+    for line in f.readlines():
+        try:
+            # 解析每行 JSON 对象
+            print(line)
+            obj = json.loads(line)
+            # 在这里对 obj 进行处理
+            print(obj)
+            break
+        except Exception as e:
+            # 如果解析 JSON 对象时出现错误，则在这里处理异常
+            #print(f'Error: {e}')
+            pass
+    
+
+
+# if rank == 0:
+#     data = {'a': 7, 'b': 3.14}
+#     comm.send(data, dest=1, tag=11)
+# elif rank == 1:
+#     data = comm.recv(source=0, tag=11)
+#     print(data)
+# else:
+#     print(rank,"idle")
