44882d39c165c53c6847f91a8bcd7ca75c086d0c
loading8425 <szej18@gmail.com>
2023-04-10 13:39:50 +1000

    Non-edit version update

--- main.py
+++ main.py
@@ -2,51 +2,123 @@ from mpi4py import MPI
 import json, ijson, os, math
 import re
 import threading
-from utils import  process_large_json_file_v3
-import queue
+from utils import  process_large_json_file_v3, data_Validation, process_large_json_file_v2
+import heapq
 import time
 
-def main():
+# Ground Truth
+# Processing....100.0% Number of data processed: 9092273 
+# [('2gmel', 2286773), ('1gsyd', 2115934), ('3gbri', 857063), ('5gper', 589115), ('4gade', 462676), ('6ghob', 90053)]
+# [('1089023364973219840', 28128), ('826332877457481728', 27581), ('1250331934242123776', 25247), ('1423662808311287813', 21030), ('1183144981252280322', 20656), ('820431428835885059', 20063), ('1270672820792508417', 19801), ('233782487', 18179), ('84958532', 15676), ('719139700318081024', 15314)]
+# time spend: 130.86579751968384
+
+# json processing with ijson mod(file editing needed)
+def ijsonMain():
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
     size = comm.Get_size()
     filename = "bigTwitter.json"
-
+    top_k = 10
     # calculate chunk for each nodes
     file_size = os.path.getsize(filename)
-    chunk_size = math.ceil(file_size / size)
-    start = rank * chunk_size
-    end = min(file_size, start + chunk_size)
+    chunk_num = math.ceil(file_size / 65536)       # config specifically for ijson
+    chunk_num_each_node = int(chunk_num/size)           # each node process chunks with size of 65535
+    if chunk_num_each_node < 1:
+        chunk_num_each_node = 1
+    start = rank * chunk_num_each_node*65536
+    end = start + chunk_num_each_node*65537
+
     #--------------------First Node--------------------#
     if rank == 0:
-        t1 = time.time()
         print("Program running on size of %d core"% (size))
 
         if size == 1:
-            num_objects = 0
-            location_tweets_count_dict = process_large_json_file_v3(filename,0)
+            location_tweets_count_dict,_,id_count = process_large_json_file_v3(filename,0)
+            print()
             print(sorted(location_tweets_count_dict.items(), key=lambda x:x[1], reverse=True))
+            k_most_common = heapq.nlargest(top_k, id_count.items(), key=lambda x: x[1])
+            print(k_most_common)
 ################################################ multi nodes #############################################
         else:
-            location_tweets_count_dict = process_large_json_file_v3(filename,start,end)
-            
+            location_tweets_count_dict, visited, id_count = process_large_json_file_v3(filename,0,end)
+            visited_dict = {rank:visited}
             for r in range(1,size):
                 data = comm.recv(source=r)
-                for key, value in data.items():
-                    if key not in data.keys():
+                visited_list = data[1]
+                visited_dict[r] = visited_list
+                id_count += data[2]
+                for key, value in data[0].items():
+                    if key not in data[0].keys():
                         location_tweets_count_dict[key] = value
                     else:
                         location_tweets_count_dict[key] += value
-            print(sorted(location_tweets_count_dict.items(), key=lambda x:x[1], reverse=True))
-        print(time.time()-t1)
+            
+            # processing data that is missed or duplicated
+            location_tweets_count_dict, id_count = data_Validation(filename,visited_dict, location_tweets_count_dict, id_count)
+            location_tweets_count_dict = sorted(location_tweets_count_dict.items(), key=lambda x:x[1], reverse=True)
+            print(location_tweets_count_dict,'\n')
+
+            # get top k number of tweets
+            k_most_common = heapq.nlargest(top_k, id_count.items(), key=lambda x: x[1])
+            print(k_most_common)
 
     #--------------------Other Node--------------------#
     else:
-        location_tweets_count_dict = process_large_json_file_v3(filename,start,end)
+        location_tweets_count_dict, visited_list, id_count = process_large_json_file_v3(filename,start,end)
         
         #print("Core %d done, total number of objects = %d, results:"% (rank, num_objects), location_tweets_count_dict)
         # send back processed data back to root
-        comm.send(location_tweets_count_dict, dest=0)
+        comm.send([location_tweets_count_dict, visited_list, id_count], dest=0)
+
+# json processing without editing file
+def jsonMain():
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    size = comm.Get_size()
+    filename = "bigTwitter.json"
+    top_k = 10
+    # calulate chunkc for each nodes
+    file_size = os.path.getsize(filename)
+    chunk_size = math.ceil(file_size / size)
+    start = rank * chunk_size
+    end = start + chunk_size
+
+    #--------------------First Node--------------------#
+    if rank == 0:
+        print("Program running on size of %d core"% (size))
+        location_tweets_count_dict, visited, id_count = process_large_json_file_v2(filename,0,end)
+        print()
+        visited_dict = {rank:visited}
+        for r in range(1,size):
+            data = comm.recv(source=r)
+            visited_list = data[1]
+            visited_dict[r] = visited_list
+            id_count += data[2]
+            for key, value in data[0].items():
+                if key not in data[0].keys():
+                    location_tweets_count_dict[key] = value
+                else:
+                    location_tweets_count_dict[key] += value
+        
+        # processing data that is missed or duplicated
+        #location_tweets_count_dict, id_count = data_Validation(filename,visited_dict, location_tweets_count_dict, id_count)
+        location_tweets_count_dict = sorted(location_tweets_count_dict.items(), key=lambda x:x[1], reverse=True)
+        print(location_tweets_count_dict,'\n')
+
+        # get top k number of tweets
+        k_most_common = heapq.nlargest(top_k, id_count.items(), key=lambda x: x[1])
+        print(k_most_common)
+
+    #--------------------Other Node--------------------#
+    else:
+        location_tweets_count_dict, visited_list, id_count = process_large_json_file_v2(filename,start,end)
+        
+        #print("Core %d done, total number of objects = %d, results:"% (rank, num_objects), location_tweets_count_dict)
+        # send back processed data back to root
+        comm.send([location_tweets_count_dict, visited_list, id_count], dest=0)
 
 if __name__ == '__main__':
-    main()
+    t1 = time.time()
+    #ijsonMain()
+    jsonMain()
+    print("time spend:",time.time()-t1)
--- utils.py
+++ utils.py
@@ -1,6 +1,9 @@
 import json, ijson
 import os, math
 import time, queue
+from collections import Counter
+import heapq
+
 # load sal data
 sal_filename = "sal.json"
 with open(sal_filename, 'r') as f:
@@ -30,211 +33,240 @@ def compare_city_name(place: str):
     return -1
 
 def find_next_start_json(file_path, start):
-    with open(file_path, 'r', encoding='utf-8') as file:
+    with open(file_path, 'rb') as file:
             # Move the starting point to the next complete JSON object
             file.seek(start)
             while True:
                 line = file.readline()
-                if not line:
+                if not line or line==b'':
                     return -1
-                tmp = line.strip().split(':')[0]
+                try:
+                    tmp = line.decode().strip().split(':')[0]
+                except:
+                    print("error:",line, file.tell(),"####", os.path.getsize(file_path))
                 if tmp == "\"_id\"":
                     break
             i=0
-            for c in line[::-1]:
-                if c=='{':
-                    break
+            pre_line = file.tell()-len(line)
+            file.seek(pre_line)
+            while True:
+                file.seek(file.tell()-2)
                 i+=1
-            start = file.tell() - i - 3
+                if file.read(1) == b'{':
+                    break
+            start = pre_line - i - 1
+            file.seek(start)
+            if file.read(1) != b'{':
+                print("error:", line)
     return start
 
-# def read_one_json_obj(file_path, start)->dict:
-#     start = find_next_start_json(file_path, start)
-#     if start == -1:
-#         return None, -1
-#     with open(file_path,'r',encoding='utf-8') as file:
-#         file.seek(start)
-#         buffer = ''
-#         object_count = 0
-#         while True:
-#             line = file.readline()
-#             if not line:
-#                 break
-#             if line.strip().split(':')[0] == "\"_id\"":
-#                 object_count+=1
-#                 if object_count == 2:
-#                     break
-#             buffer += line
-#         i=0
-#         for c in buffer[::-1]:
-#             if c=='}':
-#                 break
-#             i+=1
-#         buffer = buffer[:len(buffer)-i]
-#         js=json.loads(buffer)
-#         end = file.tell()-len(line) - 4
-#     return js, end
+def read_one_json_obj(file_path, start)->list:
+    if start <= -1:
+        return None, -1
+    start = find_next_start_json(file_path, start)
+    with open(file_path,'r',encoding='utf-8') as file:
+        file.seek(start)
+        buffer = ''
+        object_count = 0
+        while True:
+            line = file.readline()
+            if not line:
+                break
+            if line.strip().split(':')[0] == "\"_id\"":
+                object_count+=1
+                if object_count == 2:
+                    break
+            buffer += line
+        i=0
+        for c in buffer[::-1]:
+            if c=='}':
+                break
+            i+=1
+        buffer = buffer[:len(buffer)-i]
+        js=json.loads(buffer)
+        end = file.tell()-len(line) - 4
+    return js, end
 
-# def read_json_file(file_path, file_start, file_end):
-#     dic_list = []
-#     start = file_start
-#     end = file_start
-#     num_obj = 0
-#     while True:
-#         objs,end = read_one_json_obj(file_path, start)
-#         if objs==None:
-#             break
-#         dic_list.append(objs)
-#         if end>=file_end:
-#             break
-#         num_obj+=1
-#         start = end
-#     #print(dic_list)
-#     #print(len(dic_list))
-#     return dic_list, end
-
-# def process_large_json_file(file_path, file_start, file_end=9999999999999):
-#     location_tweets_count_dict = {'1gsyd':0,'2gmel':0,'3gbri':0,'4gade':0,'5gper':0,'6ghob':0}
-#     with open(file_path,'r',encoding='utf-8') as file:
-#         start = find_next_start_json(file_path, file_start)
-#         file.seek(start)
-#         stop = False
-#         while not stop:
-#             buffer = ''
-#             object_count = 0
-#             # load next one json object
-#             while True:
-#                 line = file.readline()
-#                 if not line:
-#                     stop = True
-#                     break
-#                 if line.strip().split(':')[0] == "\"_id\"":
-#                     object_count+=1
-#                     if object_count == 2:
-#                         break
-#                 buffer += line
-#             # delete unwanted chars at end of json data
-#             i=0
-#             for c in buffer[::-1]:
-#                 if c=='}':
-#                     break
-#                 i+=1
-#             buffer = buffer[:len(buffer)-i]
-#             #print(buffer)
-#             #print("##############################################")
-#             data=json.loads(buffer)
-#             # processing data
-#             place = data["includes"]['places'][0]['full_name']
-#             gcc = compare_city_name(place)
-#             if gcc in location_tweets_count_dict.keys():
-#                 location_tweets_count_dict[gcc]+=1
-#             end = file.tell()-len(line) - 4
-#             if end >= file_end:
-#                 break
-#             file.seek(end)
-#     return location_tweets_count_dict
-
-# def process_large_json_file_v2(file_path, file_start, file_end=9999999999999):
-#     location_tweets_count_dict = {'1gsyd':0,'2gmel':0,'3gbri':0,'4gade':0,'5gper':0,'6ghob':0}
-#     with open(file_path,'r',encoding='utf-8') as file:
-#         start = find_next_start_json(file_path, file_start)
-#         file.seek(start)
-#         stop = False
-#         bracket_queue = queue.LifoQueue()
-#         while not stop:
-#             buffer = ''
-#             # load next one json object
-#             while True:
-#                 line = file.readline()
-#                 if not line:
-#                     stop = True
-#                     break
-#                 tmp_line = line.strip()
-#                 if tmp_line[0] == ',':
-#                     continue
-#                 if tmp_line[0] == '{' or tmp_line[-1] == '{':
-#                     bracket_queue.put('{')
-#                 elif tmp_line[0] == '}' or (tmp_line[-1] == '}' and tmp_line[-2] != '{'):
-#                     bracket_queue.get() 
-#                     if bracket_queue.qsize() == 0:
-#                         buffer += line
-#                         break
-#                 buffer += line
-#             # delete unwanted chars at end of json data
-#             i=0
-#             for c in buffer[::-1]:
-#                 if c=='}':
-#                     break
-#                 i+=1
-#             buffer = buffer[:len(buffer)-i]
-#             if stop:
-#                 break
-#             try:
-#                 data=json.loads(buffer)
-#             except:
-#                 print(buffer)
-#                 print("##############################################")
-#                 return
-#             # processing data
-#             place = data["includes"]['places'][0]['full_name']
-#             gcc = compare_city_name(place)
-#             if gcc in location_tweets_count_dict.keys():
-#                 location_tweets_count_dict[gcc]+=1
-#             end = file.tell()
-#             if end >= file_end:
-#                 break
-#     return location_tweets_count_dict
-
-def process_large_json_file_v3(file_path, file_start, file_end=9999999999999):
+# slower json read but no edit needed
+def process_large_json_file_v2(file_path, file_start, file_end=9999999999999):
+    filesize = os.path.getsize(file_path)
+    file_end=filesize
     location_tweets_count_dict = {'1gsyd':0,'2gmel':0,'3gbri':0,'4gade':0,'5gper':0,'6ghob':0}
+    visited_tweets = []
+    id_count = Counter()
+    if file_start < 0:
+        file_start = 0
+    if file_start >= filesize:
+        return None
+    with open(file_path,'rb') as file:
+        start = find_next_start_json(file_path, file_start)
+        file.seek(start)
+        stop = False
+        num_objects = 0
+        line = b's\n'
+        while not stop:
+            buffer = b''
+            # load next one json object
+            while True:
+                line = file.readline()
+                buffer += line
+                if line == b'':
+                    break
+                #print(line)
+                try:
+                    if line == b'  },\n' or line == b',\n':
+                        # delete unwanted chars at end of json data
+                        tmp = buffer.decode('UTF-8')
+                        i=0
+                        for c in tmp[::-1]:
+                            if c=='}':
+                                break
+                            i+=1
+                        tmp = tmp[:len(tmp)-i]
+                        data = json.loads(tmp)
+                        break
+                except:
+                    pass
+            last_obj = data["_id"]
+            # add first read object into visited list
+            if num_objects < 1:
+                visited_tweets.append([data["_id"], start])
+            
+            # print number of data
+            num_objects += 1
+            # print_data = "Processing...."+str(round(file.tell() / filesize, 3)*100)+"% Number of data processed: "+str(num_objects)+"current _id:"+last_obj
+            # print("\r{:3}".format(print_data),end=' ')
+
+            # extract place info
+            place = data["includes"]['places'][0]['full_name']
+            gcc = compare_city_name(place)
+            if gcc in location_tweets_count_dict.keys():
+                location_tweets_count_dict[gcc]+=1
+                id_count.update([data['data']['author_id']])
+            
+            # if end of file
+            end = file.tell()
+            if end >= file_end-1000:
+                break
+        visited_tweets.append([last_obj, file.tell()])
+
+    return location_tweets_count_dict, visited_tweets, id_count
+
+# fastest processing with ijson, but file editing is needed
+def process_large_json_file_v3(file_path, file_start, file_end=9999999999999):
+    filesize = os.path.getsize(file_path)
+    if(file_start < 0):
+        file_start = 0
+    location_tweets_count_dict = {'1gsyd':0,'2gmel':0,'3gbri':0,'4gade':0,'5gper':0,'6ghob':0}
+    id_count = Counter()
+    visited_tweets = []
     start = find_next_start_json(file_path, file_start)
     if start == -1:
         return location_tweets_count_dict
     with open(file_path,'rb+') as file:
         #print(start)
-        file.seek(start)
+        # add '[' at begining of start point for ijson read convenience
+        file.seek(start-1)
         file.write(b'[')
-        file.seek(start)
+        file.seek(start-2)
         objects = ijson.items(file, 'item')
         num_data = 0
-        last_obj = {}
-        first_obj = {}
-        rs = file.tell()
         for data in objects:
+            if num_data<1:
+                visited_tweets.append([data["_id"], start])
+            
+            # print number of data
+            # print_data = "Processing...."+str(round(file.tell() / filesize, 3)*100)+"% Number of data processed: "+str(num_data)
+            # print("\r{:3}".format(print_data),end=' ')
+
+            # if loop over file end add last json object to our results
+            if file.tell() >= file_end:
+                last_obj = data["_id"]
+                place = data["includes"]['places'][0]['full_name']
+                gcc = compare_city_name(place)
+                if gcc in location_tweets_count_dict.keys():
+                    location_tweets_count_dict[gcc]+=1
+                    id_count.update([data['data']['author_id']])
+                visited_tweets.append([last_obj, file.tell()])
+                break
             num_data+=1
-            if num_data==1:
-                first_obj = data
             # processing data
+            if type(data)!=type(dict()):
+                print(data[0],'\n',data[-1])
             place = data["includes"]['places'][0]['full_name']
             gcc = compare_city_name(place)
-            last_obj = data
             if gcc in location_tweets_count_dict.keys():
                 location_tweets_count_dict[gcc]+=1
-            end = file.tell()
-            if end >= file_end:
-                break
-        #print(num_data)
-        print(last_obj['_id'],first_obj['_id'], "end at:", file.tell(), "file_start:",file_start, "start",start)
-        file.seek(start)
-        file.write(b' ')
-    return location_tweets_count_dict
+                id_count.update([data['data']['author_id']])
+                #print(data['data']['author_id'])
+
+            # remove added '[' at begining of the file seek
+            if num_data==1:
+                curr = file.tell()
+                file.seek(start-1)
+                file.write(b' ')
+                file.seek(curr)
+    #print(num_data)
+    return location_tweets_count_dict, visited_tweets, id_count
+
+#***#
+# loop through all results from each node or core
+# find whether last end json object at one core is correct  
+# find each ignored json objects
+# edit results
+#***#
+def data_Validation(filename,data, results, id_count):
+    pre_end_data = data[0][1]
+    for rank in data:
+        if rank == 0:
+            continue
+
+        #print(data[rank])
+        start_data = data[rank][0]
+        start_id = start_data[0]
+        start = pre_end_data[1] - 75535
+        count = 0
+        flag = False
+
+        # find this whether this data is duplicated
+        duplicate_falg = False
+        if pre_end_data[0] == start_id:
+            #print('pre end id == start id')
+            duplicate_falg = True
+
+        while True:
+            tmp = start
+            obj,start = read_one_json_obj(filename, start)
+            if obj['_id'] == pre_end_data[0]:
+                #print(obj['_id'], start, "search start:", tmp, "pre end id:",pre_end_data[0])
+                flag = True
+                if duplicate_falg:
+                    #print("duplicated found")
+                    place = obj["includes"]['places'][0]['full_name']
+                    gcc = compare_city_name(place)
+                    if gcc in results.keys():
+                        results[gcc]-=1
+                        id_count[obj['data']['author_id']] -= 1
+                    break
+            elif flag:
+                count += 1
+                if obj['_id'] == start_id:
+                    #print(obj['_id'], start, "search start:", tmp, "pre end id:",pre_end_data[0], 'count', count)
+                    break
+                else:
+                    place = obj["includes"]['places'][0]['full_name']
+                    gcc = compare_city_name(place)
+                    if gcc in results.keys():
+                        results[gcc]+=1
+                        id_count[obj['data']['author_id']] += 1
+        pre_end_data = data[rank][1]
+    return results, id_count
 
 # for test purpose
 if __name__ == '__main__':
-    file_path = 'twitter-data-small.json'
-    # calculate chunk for each nodes
-    file_size = os.path.getsize(file_path)
-    chunk_size = math.ceil(file_size / 2)
-    print("file size:",file_size)
-    start = 0 * chunk_size
-    end = min(file_size, start + chunk_size)
-    print("\nnode 0 : ",start, end)
-    print(process_large_json_file_v3(file_path, start,end))
-
-    start = 1 * chunk_size
-    end = min(file_size, start + chunk_size)
-    print("\nnode 1 : ",start, end)
-    print(process_large_json_file_v3(file_path, start,end))
-
-
-    print("\n",process_large_json_file_v3(file_path, 0))
+    t1 = time.time()
+    file_path = 'smallTwitter.json'
+    start = 0
+    ans = process_large_json_file_v2(file_path, 0)
+    #print(ans)
+    print(time.time()-t1)
